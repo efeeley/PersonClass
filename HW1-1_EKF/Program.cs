d758fe406260451080c06cb5544d3b2752653235
efeeley <elisabeth.k.feeley@gmail.com>
2017-07-05 15:28:07 -0400

    Add project files.

---
+++ HW1-1_EKF/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/**************************************
+ * Name: Elisabeth Feeley
+ * Date: 7/2/2012
+ * Assignment: HW1-1_EKF (Test Person)
+ * Resources: N/A
+ * ***********************************/
+
+namespace HW1_1_EKF
+{
+    public class Person
+    {
+        //Attributes
+        private String strName;
+        private String strAddress;
+
+
+        //Methods
+        public Person()
+        {
+            strAddress = "I don't know";
+        }//end constructor
+
+        public String Name
+        {
+            get { return strName; }
+            set { strName = value; }
+        }//public String Name
+
+        public String Address
+        {
+            get { return strAddress; }
+            set { strAddress = value; }
+        }//end public String Address
+
+    }//end class Person
+
+}//end namespace HW1-1_EKF
---
+++ HW1-1_EKF/TestPerson.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HW1_1_EKF
+{
+    class TestPerson
+    {
+        static void Main(string[] args)
+        {
+            Person joe = new Person();
+
+            joe.Name = "joe";
+
+            Console.WriteLine(joe.Name);
+
+            joe.Address = "3101 Bardstown";
+
+            Console.WriteLine(joe.Address);
+
+            Console.ReadLine();
+
+            Person Lily = new Person();
+
+            //declare variables
+            string name, address;
+
+            //get user input
+            Console.Write("What is your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("What is your address: ");
+            address = Console.ReadLine();
+
+            Lily.Name = name;
+            //Lily.Address = address;
+
+            Console.WriteLine("\n" + "Your name is: " + Lily.Name);
+            Console.WriteLine("Your address is: " + Lily.Address);
+
+            Console.ReadLine();
+
+        }//end static void Main
+
+    }//end class TestPerson
+}
